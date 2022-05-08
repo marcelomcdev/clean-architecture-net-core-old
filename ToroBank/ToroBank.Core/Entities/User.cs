351d1bbb48776c9dd816f0193a28a08136bc0f08
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-08 20:43:46 -0300

    Add user entity

---
+++ ToroBank/ToroBank.Core/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ToroBank.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
