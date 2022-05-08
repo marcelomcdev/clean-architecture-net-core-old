c3bdfadc251d699ca2df7ce9b0d3df3354336fe0
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-08 20:46:25 -0300

    Add UserAsset entity

---
+++ ToroBank/ToroBank.Core/Entities/UserAsset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToroBank.Core.Entities
+{
+    public class UserAsset
+    {
+        public int Id { get; set; }
+        public int AssetId { get; set; }
+        public int UserId { get; set; }
+    }
+}
