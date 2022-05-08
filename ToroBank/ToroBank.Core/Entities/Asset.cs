286818e4f0de9a836143b7ca1731c85c044ad19c
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-08 20:47:39 -0300

    Add Asset entity

---
+++ ToroBank/ToroBank.Core/Entities/Asset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToroBank.Core.Entities
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+}
