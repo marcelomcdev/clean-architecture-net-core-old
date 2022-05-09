7f9e341a8c0aff91ec7b2f7e8a7486ee638538ef
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-08 23:16:30 -0300

    ensure transfer is not null

--- ToroBank/BankToro.Test/Application/TransferTest.cs
+++ ToroBank/BankToro.Test/Application/TransferTest.cs
@@ -7,10 +7,12 @@ namespace BankToro.Test.Application
     {
         TransferRequest mockRequest;
         User mockUser;
+        Transfer transfer;
 
         [SetUp]
         public void Setup()
         {
+            transfer = new Transfer();
             mockRequest = new TransferRequest()
             {
                 Event = "TRANSFER",
@@ -33,9 +35,46 @@ namespace BankToro.Test.Application
         }
 
         [Test]
+        public void Should_have_error_if_transfer_is_null()
+        {
+            Assert.Throws<System.NullReferenceException>(() => transfer.ValidateTransfer(null));
+        }
+
+        [Test]
         public void Should_have_a_value_transfered_above_0()
         {
-            Assert.Fail();
+            Assert.IsTrue(mockRequest.Amount > 0);
+        }
+
+        [Test]
+        public void Should_have_a_bank_in_origin_and_target()
+        {
+            Assert.IsTrue(mockRequest.Amount > 0);
+        }
+
+    }
+
+    public class Transfer
+    {
+        public bool ValidateTransfer(TransferRequest transfer)
+        {
+            try
+            {
+                if (transfer == null)
+                    throw new System.NullReferenceException("A requisição de transferência é inválida");
+
+            }
+            catch (System.NullReferenceException ex)
+            {
+                throw new System.NullReferenceException(ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception(ex.Message);
+            }
+            
+            
+            return false;
         }
     }
 
