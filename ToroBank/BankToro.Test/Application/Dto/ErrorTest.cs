499f35bd0fd3d6898f6ac7aa9a73a4bbba82497e
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-09 23:36:06 -0300

    add EF Core and add test

---
+++ ToroBank/BankToro.Test/Application/Dto/ErrorTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToroBank.Application.Dto;
+
+namespace BankToro.Test.Application.Dto
+{
+    public class ErrorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        [Test]
+        public void Should_pass_if_description_or_code_is_null()
+        {
+            Error error = new Error("401",null);
+            Assert.That(error.Code, Is.Not.Null);
+            Assert.That(error.Description, Is.Null);
+            Assert.True(error.Description is null);
+        }
+
+       
+
+    }
+}
